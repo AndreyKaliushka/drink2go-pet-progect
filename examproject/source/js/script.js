6adc26182812d1916aed80fd1caaa0ce45dff58e
AndreyKaliushka <108519026+AndreyKaliushka@users.noreply.github.com>
2023-06-22 02:37:27 +0700

    Add pet project

---
+++ examproject/source/js/script.js
@@ -0,0 +1,11 @@
+/* в этот файл добавляет скрипты*/
+import { swiper } from "./swiper.js";
+import { map, marker } from "./leaflet.js";
+import {openCloseMenu} from "./menu.js"
+
+
+
+swiper;
+map;
+marker;
+openCloseMenu();
